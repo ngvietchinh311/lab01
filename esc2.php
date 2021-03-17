1868f989d5127a6b711be9cef9ff45906e3250ec
ngvietchinh311 <58432183+ngvietchinh311@users.noreply.github.com>
2021-03-18 00:13:26 +0700

    Add files via upload

---
+++ esc2.php
@@ -0,0 +1,65 @@
+<html>
+    <head>
+        <title>Esc02</title>
+        <style type="text/css" media="screen">
+            th, td { border: 1px solid; padding: 1px;} /*use globally, impact to
+            all attributes*/
+        </style>
+    </head>
+    <body>
+        <h2 style = "text-align:center">Our Table</h2>
+        <table style="
+               width: 80%;
+               height: 15%">
+            <tr>
+                <th style="vertical-align: top;
+                    text-align: left"
+                    rowspan="3">
+                    Personal Details
+                </th>
+                <th style="text-align: center;
+                    vertical-align: top" 
+                    rowspan="3">
+                    Image
+                </th>
+            </tr>
+        </table>
+        
+        <table style="
+               width: 80%;
+               height: 50%">
+            <tr>
+                <td> Education process </td>
+                <td style="width: 80%">&nbsp;</td>
+            </tr>
+           <tr>
+                <td> Working process </td>
+                <td style="width: 80%">&nbsp;</td>
+            </tr>
+            <tr>
+                <td> Society activities </td>
+                <td style="width: 80%">&nbsp;</td>
+            </tr>
+            <tr>
+                <td> Skills </td>
+                <td style="width: 80%">&nbsp;</td>
+            </tr>
+            <tr>
+                <td> Experiences </td>
+                <td style="width: 80%">&nbsp;</td>
+            </tr>
+            <tr>
+                <td> Certifications </td>
+                <td style="width: 80%">&nbsp;</td>
+            </tr>
+            <tr>
+                <td> Hobbies </td>
+                <td style="width: 80%">&nbsp;</td>
+            </tr>
+            <tr>
+                <td> Desire </td>
+                <td style="width: 80%">&nbsp;</td>
+            </tr>
+            
+        </table>
+    </body>
