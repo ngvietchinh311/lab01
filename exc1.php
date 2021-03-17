5c2541848a691baf60a806c13bd47416ed824f59
ngvietchinh311 <58432183+ngvietchinh311@users.noreply.github.com>
2021-03-18 00:12:39 +0700

    Add files via upload

---
+++ exc1.php
@@ -0,0 +1,39 @@
+<html>
+   
+    <head>
+        <title>
+            Exc01
+        </title>
+    </head>
+    
+    <body>
+
+    <h1 style="text-align:center">Welcome to our Exercise 01</h1>
+    <hr />
+    <p>
+        &emsp; Xin chào nhóm bọn tôi gồm 3 thành viên chó thông tin chi tiết sẽ được viết ở dưới kính mong các bạn thông cảm.
+    </p>
+    <hr />
+    
+    <p>
+        Nhóm trưởng của team:<br/> &emsp; idol <b>Trần Hải Long</b>
+    </p>
+    <hr />
+    
+    <p>
+        2 thành viên còn lại trong nhóm là:<br/>
+        &emsp; Đặng Phú Khang<br/>
+        &emsp; Nguyễn Viết Chính
+    </p>
+    <hr />
+    
+    <h4>Link FB của từng thành viên trong nhóm</h4>
+    <p>
+        <i> Đặng Phú Khang </i>: <a href="https://www.facebook.com/dangphukhang.2" target="_blank">Thằng béo!</a> <br/>
+        <i> Nguyễn Viết Chính </i>: <a href="https://www.facebook.com/profile.php?id=100011591232732" target="_blank">Đẹp trai nhất nhóm.</a> <br/>
+        <i> Trần Hải Long </i>: <a href="https://www.facebook.com/profile.php?id=100005558387520" target="_blank">Long nghiện.</a> <br/>
+    </p>
+
+    </body>
+</html>
+
